1655e49f2b9a0124cb3d9db3160f6c7d0dafc792
Soltani <cbochras@amazon.com.tr>
2026-06-19 11:20:15 +0300

    GDD + Name Change

--- becoming/scenes/cassette.gd
+++
@@ -1,9 +0,0 @@
-extends Area2D
-
-signal selected(object_name)
-
-@export var object_id: String
-
-func _input_event(viewport, event, shape_idx):
-	if event is InputEventMouseButton and event.pressed:
-		emit_signal("selected", object_id)
